f41754feb3004d80c92685944b21f95cdbe6f95e
Anarchy <cardinal.florian.erwan@gmail.com>
2019-07-17 20:31:20 +0200

    Add files via upload

--- legachat.sql
+++ legachat.sql
@@ -16,19 +16,19 @@ SET time_zone = "+00:00";
 -- Structure de la table "msg"
 
 CREATE TABLE `msg` (
-  `idMsg` int(64) NOT NULL COMMENT '[int]',
-  `idOwner` int(3) NOT NULL COMMENT '[int]',
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '[timestamp]',
-  `content` varchar(512) COLLATE utf8_unicode_ci NOT NULL COMMENT '[rot13(char)]'
+	`idMsg` int(64) NOT NULL COMMENT '[int]',
+	`idOwner` int(3) NOT NULL COMMENT '[int]',
+	`time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '[timestamp]',
+	`content` varchar(512) COLLATE utf8_unicode_ci NOT NULL COMMENT '[rot13(char)]'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
 -- Structure de la table "usr"
 
 CREATE TABLE `usr` (
-  `idUsr` int(3) NOT NULL COMMENT '[int]',
-  `name` varchar(32) COLLATE utf8_unicode_ci NOT NULL COMMENT '[char]',
-  `pass` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT '[rot13(sha256(char))]',
+	`idUsr` int(3) NOT NULL COMMENT '[int]',
+	`name` varchar(32) COLLATE utf8_unicode_ci NOT NULL COMMENT '[char]',
+	`pass` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT '[rot13(sha256(char))]',
 	`address` varchar(40) COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '[char]',
 	`lastConnect` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '[timestamp]',
 	`isAdmin` tinyint(1) NOT NULL DEFAULT '0' COMMENT '[bool(0, 1)]'
@@ -44,12 +44,12 @@ INSERT INTO `usr` (`idUsr`, `name`, `pass`, `address`, `lastConnect`, `isAdmin`)
 -- Modifications pour les tables déchargées
 
 ALTER TABLE `msg`
-  ADD PRIMARY KEY (`idMsg`),
+	ADD PRIMARY KEY (`idMsg`),
 	MODIFY `idMsg` int(64) NOT NULL AUTO_INCREMENT COMMENT '[int]';
 
 ALTER TABLE `usr`
-  ADD PRIMARY KEY (`idUsr`),
-  ADD UNIQUE KEY `username` (`name`),
+	ADD PRIMARY KEY (`idUsr`),
+	ADD UNIQUE KEY `username` (`name`),
 	MODIFY `idUsr` int(3) NOT NULL AUTO_INCREMENT COMMENT '[int]',
 	AUTO_INCREMENT=2;
 COMMIT;
